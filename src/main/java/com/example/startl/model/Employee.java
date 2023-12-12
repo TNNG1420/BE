257047f5342dcc5fedf525092f535c0a6d7c89de
TanBD <tanbd@rikkeisoft.com>
2023-12-12 23:47:09 +0700

    add sql

---
+++ src/main/java/com/example/startl/model/Employee.java
@@ -0,0 +1,63 @@
+package com.example.startl.model;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+    @Column(name = "ADDRESS", nullable = false)
+    private String emailId;
+
+    public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+    //getter/setter methods
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
